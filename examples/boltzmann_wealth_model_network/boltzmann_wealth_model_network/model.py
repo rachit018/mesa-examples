1b67c1b002f7137b0329fbb5a3280c1fa148b967
Rachit Verma <vermarachit854@gmail.com>
2026-03-22 02:49:43 +0530

    Add configurable max initial wealth parameter

--- examples/boltzmann_wealth_model_network/boltzmann_wealth_model_network/model.py
+++ examples/boltzmann_wealth_model_network/boltzmann_wealth_model_network/model.py
@@ -9,8 +9,9 @@ from .agents import MoneyAgent
 class BoltzmannWealthModelNetwork(Model):
     """A model with some number of agents."""
 
-    def __init__(self, n=7, num_nodes=10, rng=None):
+    def __init__(self, n=7, num_nodes=10, rng=None, max_wealth=5):
         super().__init__(rng=rng)
+        self.max_wealth = max_wealth
 
         self.num_agents = n
         self.num_nodes = num_nodes if num_nodes >= self.num_agents else self.num_agents
